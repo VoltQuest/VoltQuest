deaafbcde36efef238f09f1691d9fa1af407f8c6
shadow <shadow-foss@protonmail.com>
2026-03-09 08:54:28 +0530

    remove emscripten code, will be replaced by better implementiation

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,7 @@
 #include "../include/ui_utils.hpp"
 #include "../include/window_manager.hpp"
 #include "raylib.h"
-#include <cstdio>
 
-#if defined(EMSCRIPTEN)
-#include <emscripten/emscripten.h>
-#endif
-
-void gameLoop() {
-  if (!globalSettings.isGameRunning) {
-#if defined(EMSCRIPTEN)
-    emscripten_cancel_main_loop(); // stop main loop when exiting
-#endif
-    return;
-  }
-
-  drawCurrentScreen();
-}
 
 int main() {
   initBasePath();
@@ -31,18 +16,12 @@ int main() {
   createWindow();
   calculateScreenScale();
   updateLayout();
-
   TextureManager::LoadSVG("voltquest_logo",
                           getResourcePath("assets/logos/voltquest.svg"),
                           safeScreenScale);
-#if defined(EMSCRIPTEN)
-  emscripten_set_main_loop(gameLoop, 0, 1);
-#else
   while (globalSettings.isGameRunning) {
-    gameLoop();
+    drawCurrentScreen();
   }
-#endif
-
   CloseWindow();
   return 0;
 }
--- src/window_manager.cpp
+++ src/window_manager.cpp
@@ -3,7 +3,6 @@
 #include "raylib.h"
 
 void createWindow() {
-#ifndef EMSCRIPTEN
   SetConfigFlags(FLAG_VSYNC_HINT);
   SetConfigFlags(FLAG_MSAA_4X_HINT);
   InitWindow(0, 0, "VoltQuest");
@@ -25,15 +24,6 @@ void createWindow() {
 
   // Create a the game window
   SetWindowSize(globalSettings.screenWidth, globalSettings.screenHeight);
-
-#else
-  globalSettings.screenWidth = 960;
-  globalSettings.screenHeight = 540;
-  globalSettings.fullscreen = false;
-  globalSettings.refreshRate = 60;
-  InitWindow(globalSettings.screenWidth, globalSettings.screenHeight,
-             "VoltQuest");
-#endif
 
   SetExitKey(0); // Disables Escape key from CloseWindow
   SetTargetFPS(globalSettings.refreshRate);
